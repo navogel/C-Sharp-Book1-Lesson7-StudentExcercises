c2447cc6b3c05e3443c748bad954e21719043c00
Nate V <navogel@gmail.com>
2019-12-06 15:24:52 -0600

    Added SQL query to add to DB

---
+++ SQLQuery-student-exercises.sql
@@ -0,0 +1,42 @@
+﻿--CREATE TABLE Cohort (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    Name VARCHAR(55) NOT NULL
+--);
+
+--CREATE TABLE Excercise (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    Name VARCHAR(55) NOT NULL,
+--    Language VARCHAR(55) NOT NULL
+--);
+
+--CREATE TABLE Student (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    FirstName VARCHAR(55) NOT NULL,
+--    LastName VARCHAR(55) NOT NULL,
+--    SlackHandle VARCHAR(55) NOT NULL,
+--    CohortId INTEGER,
+--    CONSTRAINT FK_Student_Cohort FOREIGN KEY(CohortId) REFERENCES Cohort(Id)
+
+--);
+
+--CREATE TABLE Instructor (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    FirstName VARCHAR(55) NOT NULL,
+--    LastName VARCHAR(55) NOT NULL,
+--    SlackHandle VARCHAR(55) NOT NULL,
+--    Specialty VARCHAR(55) NOT NULL,
+--    CohortId INTEGER,
+--    CONSTRAINT FK_Instructor_Cohort FOREIGN KEY(CohortId) REFERENCES Cohort(Id)
+
+--);
+
+--CREATE TABLE StudentExercise (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    StudentId INTEGER,
+--    ExerciseId INTEGER,
+--    CONSTRAINT FK_StudentExcercise_Student FOREIGN KEY(StudentId) REFERENCES Student(Id),
+--    CONSTRAINT FK_StudentExcercise_Exercise FOREIGN KEY(ExerciseId) REFERENCES Excercise(Id)
+
+
+--);
+
--- Student.cs
+++ Student.cs
@@ -12,8 +12,6 @@ namespace StudentExercises
             SlackHandle = slacky;
             StudentCohort = cohorty;
             
-
-            
         }
 
        
