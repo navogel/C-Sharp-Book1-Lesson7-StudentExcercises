157a9753d4d11b4c913869eb4dffa5855bd11db1
Nate V <navogel@gmail.com>
2019-12-09 09:27:11 -0600

    added sql to Db

--- SQLQuery-student-exercises.sql
+++ SQLQuery-student-exercises.sql
@@ -36,7 +36,18 @@
 --    ExerciseId INTEGER,
 --    CONSTRAINT FK_StudentExcercise_Student FOREIGN KEY(StudentId) REFERENCES Student(Id),
 --    CONSTRAINT FK_StudentExcercise_Exercise FOREIGN KEY(ExerciseId) REFERENCES Excercise(Id)
-
-
 --);
+
+
+INSERT INTO Cohort (Name) VALUES ('Cohort 35');
+INSERT INTO Cohort (Name) VALUES ('Cohort 36');
+INSERT INTO Cohort (Name) VALUES ('Cohort 37');
+INSERT INTO Cohort (Name) VALUES ('Cohort 34');
+
+INSERT INTO Exercise (Name, Language) VALUES ('Heist', 'C#');
+INSERT INTO Exercise (Name, Language) VALUES ('Student Exercises', 'C#');
+INSERT INTO Exercise (Name, Language) VALUES ('Journal', 'JS');
+INSERT INTO Exercise (Name, Language) VALUES ('Welcome to Nashville', 'JS');
+
+
 
